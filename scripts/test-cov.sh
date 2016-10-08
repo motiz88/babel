d6745e991bc783e23f5e80bc704543b4fa7feb09
Moti Zilberman <motiz88@gmail.com>
2016-10-08 11:22:38 +0300

    [experimental] add json reporter in istanbul-combine

--- scripts/test-cov.sh
+++ scripts/test-cov.sh
@@ -7,4 +7,5 @@ scripts/_get-test-directories.sh | xargs \
  -t -P4 -n15 \
  bash -c 'node node_modules/istanbul/lib/cli.js cover --dir .coverage-$$ node_modules/mocha/bin/_mocha -- --opts test/mocha.opts "$@"'
 
-node_modules/.bin/istanbul-combine  ./.coverage-*/coverage.json
+node_modules/.bin/istanbul-combine  ./.coverage-*/coverage.json -r json && \
+  mv -f ./coverage/coverage-final.json ./coverage/coverage.json
