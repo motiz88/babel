8e97282811dbc9b8a57809f383412037d11c9230
Moti Zilberman <motiz88@gmail.com>
2016-10-08 11:06:12 +0300

    [experimental] Merge code coverage from all xargs test runs

--- scripts/test-cov.sh
+++ scripts/test-cov.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 
-scripts/_get-test-directories.sh | xargs -t -P4 -n15 node node_modules/istanbul/lib/cli.js cover node_modules/mocha/bin/_mocha -- --opts test/mocha.opts
+rm -rf .coverage-*
+
+scripts/_get-test-directories.sh | xargs \
+ -t -P4 -n15 \
+ bash -c 'node node_modules/istanbul/lib/cli.js cover --dir .coverage-$$ node_modules/mocha/bin/_mocha -- --opts test/mocha.opts "$@"'
+
+node_modules/.bin/istanbul-combine  ./.coverage-*/coverage.json
